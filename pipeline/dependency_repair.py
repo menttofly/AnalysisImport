3b122207a5770e35624b4db964c0da8dde0eb755
menttofly <1028365614@qq.com>
2021-11-13 12:53:27 +0800

    add pipeline package

---
+++ pipeline/dependency_repair.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*-coding:utf-8-*-
+
+__author__ = "zhengqi"
+
+from pipeline import Pipeine
+
+"""
+修复依赖关系
+"""
+class DependencyRepair(Pipeine):
+
+    def process(self, file: str) -> str:
+        return ""
